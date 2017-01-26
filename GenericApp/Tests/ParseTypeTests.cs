3e3dd58113f2a10dad7750e80ffb12a20bee4cf9
Jeff Darling <jeffld3@gmail.com>
2017-01-26 14:15:22 -0600

    Add files via upload

---
+++ GenericApp/Tests/ParseTypeTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericApp.Common.Tests
+{
+    [TestClass()]
+    public class ParseTypeTests
+    {
+        [TestMethod()]
+        public void ParseType_Success()
+        {
+            // Arrange
+            var TestRegex = new GenericApp.Common.RegexSamples();
+            var fileName = "1234A.TXT";
+            var expected = "A";
+
+            TestRegex.FileName = fileName;
+
+            // Act
+            var actual = TestRegex.ParseType();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
