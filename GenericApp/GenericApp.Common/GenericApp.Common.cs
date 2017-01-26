55633ae53c607e9a01424892f25a4535f4fa5e28
Jeff Darling <jeffld3@gmail.com>
2017-01-26 14:04:40 -0600

    Add files via upload

---
+++ GenericApp/GenericApp.Common/GenericApp.Common.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GenericApp.Common
+{
+    public class RegexSamples
+    {
+                
+        private string fileName;
+
+        public string FileName
+        {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+        
+        public string ParseType()
+        {
+
+            string pattern = @".*(.)\.TXT";
+            string result;
+            result = "";
+
+            Regex r = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            Match m = r.Match(fileName);
+
+            if (m.Success)
+            {
+                result = m.Groups[1].Value;
+            }
+
+            return result;
+        }
+
+    }
+}
